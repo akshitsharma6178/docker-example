4f672fb556270669119cc030c339f887b5d3965f
akshitsharma6178 <hsasakshit@gmail.com>
2023-07-31 11:27:12 -0600

    garbage collection fix

--- server/scripts/main.py
+++ server/scripts/main.py
@@ -7,25 +7,19 @@ from PIL import Image
 import io
 import gc
 
-# Load the model
 script_dir = os.path.dirname(os.path.realpath(__file__))
 model_dir = os.path.join(script_dir, '../models/ssd-mobile-net-v2/saved_model')
 loaded_model = tf.saved_model.load(model_dir)
 
 def object_detection(img):
-    
-    # Change RGBA to RGB
     if img.shape[2] == 4:
         img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB)
 
-    # Load image
     image_np = np.array(img)
 
-    # Convert image to tensor
     input_tensor = tf.convert_to_tensor(image_np)
     input_tensor = input_tensor[tf.newaxis, ...]
 
-    # Perform detection
     output_dict = loaded_model(input_tensor)
     del input_tensor
 
@@ -35,18 +29,16 @@ def object_detection(img):
     output_dict['num_detections'] = num_detections
 
     output_dict['detection_classes'] = output_dict['detection_classes'].astype(np.int64)
-
-    # Draw the bounding boxes on the image
+    
+    gc.collect()
     for i in range(num_detections):
-        if output_dict['detection_scores'][i] > 0.5:  # only keep detections with confidence above 0.5
+        if output_dict['detection_scores'][i] > 0.5:
             ymin, xmin, ymax, xmax = output_dict['detection_boxes'][i]
             (left, right, top, bottom) = (xmin * img.shape[1], xmax * img.shape[1], 
                                         ymin * img.shape[0], ymax * img.shape[0])
             left, right, top, bottom = int(left), int(right), int(top), int(bottom)
-            # Draw the bounding box
             cv2.rectangle(img, (left, top), (right, bottom), (0, 255, 0), 2)
     return img
-    gc.collect()
 
 if __name__ == '__main__':
     image_data = sys.stdin.buffer.read()
